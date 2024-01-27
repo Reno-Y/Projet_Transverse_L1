c7f8f5f6490d2fef678562a73bb6ba343a1cc351
Renaud <renaud.yoganathan@efrei.net>
2024-01-28 00:52:27 +0100

    assets and music

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-
+from Class import Startup
 pygame.init()
 
 width, height = pygame.display.Info().current_w, pygame.display.Info().current_h
@@ -8,6 +8,8 @@ pygame.display.set_caption('Iron Lung')
 # récupère la taille de l'écran et affiche une fenêtre
 clock = pygame.time.Clock()
 
+gameIcon = pygame.image.load('Assets/menu/Shinobi_studio.png')
+pygame.display.set_icon(gameIcon)
 
 run = True
 while run:
@@ -15,6 +17,23 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    Startup()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     pygame.display.update()
     clock.tick(60) #limite les fps à 60
