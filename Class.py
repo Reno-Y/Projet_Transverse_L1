fedb97fbc9585e0e6f22dc2efb27a79d9383ea33
Renaud <renaud.yoganathan@efrei.net>
2024-01-28 00:57:53 +0100

    assets and music

--- Class.py
+++ Class.py
@@ -2,5 +2,6 @@ import pygame
 
 
 class Startup:
-    studio_logo = pygame.image.load("Assets/Shinobi_studio.png")
+    pygame.mixer.init()
+    studio_logo = pygame.image.load("Assets/menu/Shinobi_studio.png")
     studio_sound = pygame.mixer.Sound("sound/music/startup.mp3")
